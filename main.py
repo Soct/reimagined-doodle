e0aacc58577d0987aa8f5f0bc936e8fcbc48e433
Tchoupinax <corentinfiloche@hotmail.fr>
2025-07-27 16:24:51 +0200

    ok

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ async def help(ctx: discord.ApplicationContext):
 
 @bot.slash_command(name="tchoupinax", description="Say hello to the bot")
 async def help(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey2!")
+    await ctx.defer()
+
+    channel = discord.utils.get(ctx.guild.voice_channels, name="Vocal")
+
+    if not channel:
+        await ctx.respond("Channel 'Vocal' not found.")
+        return
+
+    members_array = []
+    for member in ctx.guild.members:
+        members_array.append(member.display_name)
+
+    await ctx.respond(f"Members in #Vocal2: {', '.join(members_array)}")
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
